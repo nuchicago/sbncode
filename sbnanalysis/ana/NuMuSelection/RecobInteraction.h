efb1c30033b2e0cf8ce783faf4fee237fedf660e
Gray Putnam <grayputnam@uchicago.edu>
2018-07-30 17:05:35 -0500

    add in custom cpp class for reconstruciton event info and generate ROOT dictionary

---
+++ sbnanalysis/ana/NuMuSelection/RecobInteraction.h
@@ -0,0 +1,24 @@
+#ifndef __sbnanalysis_NuMuSelection_RecobInteraction__
+#define __sbnanalysis_NuMuSelection_RecobInteraction__
+
+/**
+ * \file RecobInteraction.hh
+ */
+
+#include <map>
+#include <string>
+#include <vector>
+#include <TTree.h>
+#include <TVector3.h>
+
+/**
+ * \class RecobInteraction
+ */
+class RecobInteraction {
+public:
+  int l_pandora_pdgid;
+  double l_mcs_momentum;
+};
+
+#endif  // __sbnanalysis_core_Event__
+
