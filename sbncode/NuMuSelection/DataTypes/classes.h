5805d0edb88d5d3f19d6cd0e56eb78c978019c3c
Gray Putnam <grayputnam@uchicago.edu>
2018-08-14 10:02:25 -0500

    add all typedefs

--- sbncode/NuMuSelection/DataTypes/classes.h
+++ sbncode/NuMuSelection/DataTypes/classes.h
@@ -12,5 +12,7 @@ namespace {
     art::Wrapper<std::vector<numuselection::AssocTruthInfo>> w_v_ato;
     art::Assns<numuselection::AssocTruthInfo,simb::MCTruth, void> a_ato_mct;
     art::Wrapper<art::Assns<numuselection::AssocTruthInfo,simb::MCTruth, void>> w_a_ato_mct;
+    art::Assns<simb::MCTruth,numuselection::AssocTruthInfo, void> a_mct_ato;
+    art::Wrapper<art::Assns<simb::MCTruth,numuselection::AssocTruthInfo, void>> w_a_mct_ato;
   };
 }
