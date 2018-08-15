e5d8271cec67bbcac2d38d8ec25bc3fbb01956f5
Gray Putnam <grayputnam@uchicago.edu>
2018-08-15 11:58:06 -0500

    add in ECCQE utility function

--- sbnanalysis/ana/SBNOsc/Utilities.h
+++ sbnanalysis/ana/SBNOsc/Utilities.h
@@ -26,6 +26,10 @@ void hello();
 /** Extract truth information to approximate reconstruction. */
 Event::Interaction TruthReco(const simb::MCTruth& mctruth);
 
+/** Calculate CCQE energy from associated lepton information (and optional distortion). Energy in GeV. */
+double ECCQE(const TVector3& l_momentum, double l_energy, double energy_distortion=0., double angle_distortion=0.);
+
+
   }  // namespace SBNOsc
 }  // namespace ana
 
