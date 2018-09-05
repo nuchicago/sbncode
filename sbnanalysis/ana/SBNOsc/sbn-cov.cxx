3e4d5e1a2e1fa9b5f9fab7b2dcad2d44ec2fd4e1
Gabriel Ansarah Varela <Biel@Gabriels-MacBook-Pro.local>
2018-09-05 16:25:09 -0500

    Changing to fit Gray's selection

--- sbnanalysis/ana/SBNOsc/sbn-cov.cxx
+++ sbnanalysis/ana/SBNOsc/sbn-cov.cxx
@@ -13,47 +13,6 @@
 #include <TFile.h>
 #include <TCanvas.h>
 
-#include <TChain.h>
-#include <TFileCollection.h>
-#include <TTreePlayer.h>
-#include <fstream>
-
-double getPOTs(std::string filelist) {
-    
-    /* Stolen from Swapnil */
-    
-    TChain chain("SubRuns");
-    TFileCollection f("dum");
-    f.AddFromFile(filelist.c_str());
-    chain.AddFileInfoList(f.GetList());
-    ((TTreePlayer*)(chain.GetPlayer()))->SetScanRedirect(true);
-    ((TTreePlayer*)(chain.GetPlayer()))->SetScanFileName("total_pot.list");
-    chain.Scan("sumdata::POTSummary_generator__GenieGen.obj.totpot");
-    
-    
-    std::ifstream input("total_pot.list");
-    double total = 0.0;
-    
-    std::string line;
-    while(std::getline(input,line)) {
-        
-        if (line.empty()) continue;
-        std::istringstream is(line);
-        
-        std::string dim1, dim2, dim3;
-        int row;
-        double pot;
-        if (is >> dim1 >> row >> dim2 >> pot >> dim3) { total += pot; }
-        
-    }
-    
-    //remove((std::string)"total_pot.list");
-    
-    //std::cout << "Total Exposure in ICARUS = " << total << std::endl;
-    return total;
-    
-}
-
 int main(int argc, char* argv[]) {
     
     std::cout << std::endl << "Hello!" << std::endl << std::endl;
@@ -61,39 +20,25 @@ int main(int argc, char* argv[]) {
     //// Build sample (temporary)
     //// ~~~~~~~~~~~~~~~~~~~~~~~~
     
-    std::vector <std::string> DETLIST = {"SBND", "MicroBooNE", "ICARUS"},
-                              detlist = {"sbnd", "uboone", "icarus"},
-                             desclist = {"#nu_{#mu}", "#nu_{#mu}", "#nu_{#mu}"};
+    char *configFileName = argv[1];
     
-    //std::string prelist = "/sbnd/data/users/gavarela/selection/", postlist = "/spatel_output/spatel.list";
-    //std::vector <float> scalelist = {getPOTs(prelist+"sbnd"+postlist), getPOTs(prelist+"uboone"+postlist), getPOTs(prelist+"icarus"+postlist)};
+    Json::Value* config = core::LoadConfig(configFileName);
+    assert(config != NULL);
     
-    std::vector <float> scalelist = {3.0958e18, 8.87435e19, 6.59165e18};
     std::vector <ana::SBNOsc::EventSample> samples;
     
-    std::vector <TFile*> tfiles;
-    for (int d = 0; d < detlist.size(); d++) {
+    for (auto sample : (*config)["EventSamples"]) {
         
-        std::string det = detlist[d], DET = DETLIST[d], desc = desclist[d];
-        float scale = scalelist[d];
+        std::string filename = sample["path"].asString(),
+                         det = sample["det"].asString(),
+                        desc = sample["desc"].asString();
+        float scalefactor = sample["scalefactor"].asFloat();
         
-        tfiles.push_back(new TFile(((std::string)"/sbnd/data/users/gavarela/selection/new/output_" + DET + (std::string)"_new.root").c_str()));
-        
-        samples.push_back(ana::SBNOsc::EventSample(tfiles[d], (TTree*)tfiles[d]->Get("sbnana"), scale, det, desc));
+        samples.push_back(ana::SBNOsc::EventSample(filename, scalefactor, det, desc));
         
     }
     
     assert(!samples.empty());
-    
-    //// Get configuration file
-    //// ~~~~~~~~~~~~~~~~~~~~~~
-    
-    // TODO: configure this from command line
-    
-    std::string str_configFileName = "/sbnd/app/users/gavarela/sbncode-v06_80_00/srcs/sbncode/sbnanalysis/ana/SBNOsc/config/CovarianceConfig.json";
-    
-    char *configFileName = new char[str_configFileName.size() +1];
-    strcpy(configFileName, str_configFileName.c_str());
     
     
     //// Get covariances and write outputs to ROOT file(s)
