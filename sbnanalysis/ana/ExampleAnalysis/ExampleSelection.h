50dc10dcd90a299f2a84dd25d7ed459ea481793d
Gabriel Ansarah Varela <Biel@Gabriels-MacBook-Pro.local>
2018-09-05 11:58:53 -0500

    Changed stuff in ExampleAnalysis to see how my old code goes with the 'new' selection

--- sbnanalysis/ana/ExampleAnalysis/ExampleSelection.h
+++ sbnanalysis/ana/ExampleAnalysis/ExampleSelection.h
@@ -1,84 +1,85 @@
 #ifndef __sbnanalysis_ana_ExampleAnalysis_ExampleSelection__
 #define __sbnanalysis_ana_ExampleAnalysis_ExampleSelection__
 
-/**
- * \file ExampleSelection.h
- *
- * An example event selection processor.
- *
- * This is an implementation of the core::SelectionBase class. We define
- * the methods called for initialization, finalization, and event-by-event
- * processing.
- *
- * Author: A. Mastbaum <mastbaum@uchicago.edu>
- */
-
-// Includes
+/* Includes */
 #include <iostream>
 #include "canvas/Utilities/InputTag.h"
 #include "core/SelectionBase.hh"
-#include "core/Event.hh"
+#include <vector>
+#include <string>
 
-// Forward declarations
+/* Forward declarations */
 class TH2D;
+class TH1D;
+class THStack;
+class TAttFill;
 
 /** All analysis code is defined in namespace "ana" */
 namespace ana {
+/** Code specific to the ExampleAnalysis. */
+namespace ExampleAnalysis {
 
-  /** Code specific to the ExampleAnalysis. */
-  namespace ExampleAnalysis {
-
-/**
- * \class ExampleSelection
- * \brief An example selection analysis
- *
- * This selection analysis doesn't actually select events, it just
- * demonstrates the framework!
- */
 class ExampleSelection : public core::SelectionBase {
-public:
-  /** Constructor. */
-  ExampleSelection();
+    
+    public:
+    
+    ExampleSelection();
+    void Initialize(Json::Value* config=NULL);
+    void Finalize();
+    bool ProcessEvent(gallery::Event& ev);
 
-  /**
-   * Initialization.
-   *
-   * Here we load configuration parameters, set up histograms for output, and
-   * add our own branches to the output tree.
-   *
-   * \param config A configuration, as a JSON object
-   */
-  void Initialize(Json::Value* config=NULL);
+    protected:
+    
+    unsigned fEventCounter;         //!< Count processed events
 
-  /** Finalize and write objects to the output file. */
-  void Finalize();
+    /** Configuration parameters */
+    art::InputTag fTruthTag;        //!< art tag for MCTruth information
+    art::InputTag fPartTag;   // for use with particle (non-nu) information
+    art::InputTag fEventWgtTag;
+    art::InputTag fFluxWgtTag;
 
-  /**
-   * Process one event.
-   *
-   * \param ev A single event, as a gallery::Event
-   * \param reco Reconstructed interactions
-   * \return True to keep event
-   */
-  bool ProcessEvent(const gallery::Event& ev, std::vector<Event::Interaction>& reco);
-
-protected:
-  unsigned fEventCounter;  //!< Count processed events
-
-  /** Configuration parameters */
-  art::InputTag fTruthTag;  //!< art tag for MCTruth information
-  int fMyParam;  //!< A parameter from the configuration file
-
-  /** Custom data branches */
-  int fNuCount;  //!< Number of neutrino interactions in the event
-  int fMyVar;  //!< Another variable of interest
-
-  /** Histograms */
-  TH2D* fNuVertexXZHist;  //!< Neutrino vertex XZ projection
+    /** Custom data branches */
+    //int fNuCount;  //!< Number of neutrino interactions in the event
+    
+    /** My stuff */
+    
+    // Detector
+    std::string det;
+    
+    // Particle properties
+    double ml, mn, mp, Eb;
+    
+    // Weights and universes
+    int n_unis;
+    
+    // Reason to pass cut
+    int nucount, nuindet, mu_passes, pi_passes, double_pass;
+    
+    // Histograms
+    std::vector <std::string> unis1, unis2;
+    //double min_nuE, max_nuE, nuE_interval, num_nuE_bins;
+    
+    THStack *len_stack, *count_stack;
+    std::vector <TH1D*> len_hists;
+    std::vector <std::vector <TH1D*> > nuE_hists;
+    
+    /* Note: output from the ProcessEvent stage will be (let n index neutrinos from 0 to N and u index universes from 0 to U):
+    1. nuE: vector of length N containing energies of detected neutrinos
+    2. why_pass: vector of length N containing info on why that detected neutrino passed the cut (0 if OK, 1 if because of long-tracked pion, i.e. mistake)
+    3. weights: vector of length N of vectors of length U, element weights[n][u] containing the weight corresponding to neutrino n for universe u.
+    */
+    
+    // TestCuts
+    int numucount, numu_CC, numu_CC_lessthanmu, nuV_indet;
+    int nu_passcut1, nu_passcut2, nu_passdouble;
+    int mu_pass1, mu_pass2, pi_pass1, pi_pass2;
+    int mu_contained, mu_escaped, pi_contained, pi_escaped;
+    TH1D *mu_len_contained, *mu_len_escaped, *pi_len_contained, *pi_len_escaped;
+    THStack *contained, *escaped;
 };
 
-  }  // namespace ExampleAnalysis
-}  // namespace ana
+}// namespace ExampleAnalysis
+}// namespace ana
 
-#endif  // __sbnanalysis_ana_ExampleAnalysis_ExampleSelection__
+#endif// __sbnanalysis_ana_ExampleAnalysis_ExampleSelection__
 
