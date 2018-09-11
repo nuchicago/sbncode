bb828c000ab2f37a4c761c33b55308c59ea4ff20
gputnam <gputnam@sbndgpvm01.fnal.gov>
2018-09-11 13:19:55 -0500

    Revert: 34abe2fc09c275b979398234475c2160b54427b3 Remove handling art exceptions

--- sbnanalysis/core/ProcessorBlock.cxx
+++ sbnanalysis/core/ProcessorBlock.cxx
@@ -27,42 +27,16 @@ void ProcessorBlock::ProcessFiles(std::vector<std::string> filenames) {
 
   // Event loop
   for (gallery::Event ev(filenames); !ev.atEnd(); ev.next()) {
-    for (unsigned processor_ind = 0; processor_ind < fProcessors.size(); processor_ind++) {
-      auto &it = fProcessors[processor_ind];
+    for (auto it : fProcessors) {
       it.first->BuildEventTree(ev);
 
-      // catch an art exceptions
-      bool accept = false;
-      try {
-        accept = it.first->ProcessEvent(ev, it.first->fEvent->truth, *it.first->fReco);
-      }
-      catch (cet::exception e) {
-        // in the event of an exception, gracefully fill the processor
-        std::cerr << "Caught art exception. Closing and removing processor." << std::endl;
-        std::cerr << e.explain_self() << std::endl;
-
-        it.first->EventCleanup();
-        it.first->Finalize();
-        it.first->Teardown();
-        fProcessors.erase(fProcessors.begin() + processor_ind);
-        // need to bump back the index to account for erased processor
-        //
-        // unsigned underflow/overflow arithmetic is well-defined, so this is ok even if processor_ind == 0
-        processor_ind --;
-        // to the next processor
-        continue;
-      }
+      bool accept = it.first->ProcessEvent(ev, it.first->fEvent->truth, *it.first->fReco);
 
       if (accept) {
         it.first->FillTree();
       }
 
       it.first->EventCleanup();
-    }
-    // finish if no processors are left
-    if (fProcessors.size() == 0) {
-      std::cerr << "No remaining processors. Exiting." << std::endl;
-      break;
     }
   }
 
